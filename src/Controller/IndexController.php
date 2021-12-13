cb897f67addffb856ffda686bd74cf24259ac7cd
Shohei Ito <skytree.skysky@gmail.com>
2021-12-13 21:38:40 +0900

    json

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,8 +11,6 @@ class IndexController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->json(['username' => 'test']);
     }
 }
