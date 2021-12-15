a0573b4505055204425085400ade3a3e5a08f0b4
Shohei Ito <skytree.skysky@gmail.com>
2021-12-15 16:52:39 +0900

    bundle追加

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Subject;
 use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,12 +15,20 @@ class IndexController extends AbstractController
      * @throws \Exception
      */
     #[Route('/', name: 'index')]
-    public function index(SubjectRepository $subjectRepository): Response
+    public function index(Request $request,SubjectRepository $subjectRepository): Response
     {
-        $subs = $subjectRepository->findAll();
+        //0かクエリの数値の最大数がセットされる
+        $offset = max(0, $request->query->getInt('offset'));
+        $subs = $subjectRepository->getPagination($offset);
+
+
+//        $subs = $subjectRepository->findAll();
 
         return $this->render('index/index.html.twig',[
-            'subs' => $subs
+//            'subject' => $subject,
+            'subs' => $subs,
+            'previous' => $offset - SubjectRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($subs), $offset + SubjectRepository::PAGINATOR_PER_PAGE)
         ]);
     }
 }
