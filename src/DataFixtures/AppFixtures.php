a07620474048e1490f34067051d0372de5b161f6
Shohei Ito <skytree.skysky@gmail.com>
2021-12-16 10:46:38 +0900

    ページングテスト

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,7 +11,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
 
-        for ($i = 0; $i < 10; $i++){
+        for ($i = 1; $i <= 20; $i++){
             $sub = new Subject();
             $sub->setTitle('test_'. $i);
             $manager->persist($sub);
