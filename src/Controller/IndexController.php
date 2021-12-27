904507a7f0d04a024d3fa39719e0dbab42826397
Shohei Ito <skytree.skysky@gmail.com>
2021-12-27 13:08:58 +0900

    修正

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,29 +11,29 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/test')]
+//#[Route('/test')]
 class IndexController extends AbstractController
 {
     /**
      * @throws \Exception
      */
-    #[Route('/', name: 'index')]
-    public function index(Request $request,SubjectRepository $subjectRepository): Response
-    {
-        //0かクエリの数値の最大数がセットされる
-        $offset = max(0, $request->query->getInt('offset'));
-        $subs = $subjectRepository->getPagination($offset);
-
-
-//        $subs = $subjectRepository->findAll();
-
-        return $this->render('index/index.html.twig',[
-//            'subject' => $subject,
-            'subs' => $subs,
-            'previous' => $offset - SubjectRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($subs), $offset + SubjectRepository::PAGINATOR_PER_PAGE)
-        ]);
-    }
+//    #[Route('/', name: 'index')]
+//    public function index(Request $request,SubjectRepository $subjectRepository): Response
+//    {
+//        //0かクエリの数値の最大数がセットされる
+//        $offset = max(0, $request->query->getInt('offset'));
+//        $subs = $subjectRepository->getPagination($offset);
+//
+//
+////        $subs = $subjectRepository->findAll();
+//
+//        return $this->render('index/index.html.twig',[
+////            'subject' => $subject,
+//            'subs' => $subs,
+//            'previous' => $offset - SubjectRepository::PAGINATOR_PER_PAGE,
+//            'next' => min(count($subs), $offset + SubjectRepository::PAGINATOR_PER_PAGE)
+//        ]);
+//    }
 
     #[Route('/test', name: 'test', methods: 'GET')]
     public function test(Request $request, PaginatorInterface $paginator, SubjectRepository $subjectRepository): Response
