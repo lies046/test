4703944328dd6a1f2cb7fd5f340fd4b51d2be8bc
Shohei Ito <skytree.skysky@gmail.com>
2021-12-29 23:03:42 +0900

    session設置テスト

--- src/EventListener/RequestListener.php
+++ src/EventListener/RequestListener.php
@@ -2,12 +2,23 @@
 
 namespace App\EventListener;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
 class RequestListener
 {
+    private $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
     public function onKernelRequest(RequestEvent $event)
     {
-        dd($event->getRequest()->getMethod());
+        if($code = $event->getRequest()->get('code')){
+            $session = $this->requestStack->getSession();
+            $session->set('advertising_code', $code);
+        }
     }
 }
