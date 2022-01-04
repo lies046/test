8d1505549f436cb6f7e67a675bffff9e3ccec2b3
Shohei Ito <skytree.skysky@gmail.com>
2022-01-04 22:45:45 +0900

    IsGrantedテスト

--- src/Controller/ChangePasswordController.php
+++ src/Controller/ChangePasswordController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class ChangePasswordController extends AbstractController
 {
-    #[Route('/change/password', name: 'change_password')]
+    #[Route('/change/password', name: 'change_password'),IsGranted('ROLE_USER')]
     public function index(): Response
     {
         return $this->render('change_password/index.html.twig', [
