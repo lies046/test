234e3f279993c33aa8fc908da63e07daedb0fddf
Shohei Ito <skytree.skysky@gmail.com>
2022-01-12 22:57:42 +0900

    ajax修正

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,11 +1,13 @@
 $('.like').on('click', function (){
     let postId = $(this).attr('id');
+    let userId = $('#user_id').data('user-id');
     $.ajax({
         url: "/like",
         type: "POST",
         dataType: "json",
         data:{
-            "post_id": postId
+            "post_id": postId,
+            "user_id": userId
         }
     })
 
