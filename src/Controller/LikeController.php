1ad499dddf33ed2035e1eafd0c522855fbefe2d6
Shohei Ito <skytree.skysky@gmail.com>
2022-01-14 23:14:59 +0900

    like保存追加

--- src/Controller/LikeController.php
+++ src/Controller/LikeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Like;
 use App\Entity\Post;
 use App\Entity\User;
 use App\Repository\PostRepository;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,13 +15,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class LikeController extends AbstractController
 {
     #[Route('/like', methods: 'POST')]
-    public function likeAction(Request $request, PostRepository $postRepository, UserRepository $userRepository)
+    public function likeAction(Request $request,
+                               PostRepository $postRepository,
+                               UserRepository $userRepository,
+    EntityManagerInterface $entityManager)
     {
+        $like = new Like();
         /** @var Post $post */
         $post = $postRepository->find($request->request->get('post_id'));
-
         /** @var User $user */
         $user = $userRepository->find($request->request->get('user_id'));
+        $like->setPost($post)
+            ->setLikeUser($user);
+        $entityManager->persist($like);
+        $entityManager->flush();
         return $this->json('success');
     }
 }
